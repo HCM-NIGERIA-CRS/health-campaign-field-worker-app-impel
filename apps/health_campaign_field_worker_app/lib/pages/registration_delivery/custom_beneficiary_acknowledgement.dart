25137d17a3243544987bc89626df71beee5b10cc
satviktm7 <satvik.t@egovernments.org>
2025-05-08 19:27:07 +0530

    change made in acknowledgment, removed the household id

--- apps/health_campaign_field_worker_app/lib/pages/registration_delivery/custom_beneficiary_acknowledgement.dart
+++ apps/health_campaign_field_worker_app/lib/pages/registration_delivery/custom_beneficiary_acknowledgement.dart
@@ -101,8 +101,8 @@ class CustomBeneficiaryAcknowledgementPageState
                   type: PanelType.success,
                   title: localizations.translate(
                       i18.acknowledgementSuccess.acknowledgementLabelText),
-                  subTitle: subtitleMap(
-                      householdMemberWrapper, state.householdModel?.id),
+                  // subTitle: subtitleMap(
+                  //     householdMemberWrapper, state.householdModel?.id),
                   actions: [
                     if (householdMemberWrapper != null)
                       DigitButton(
