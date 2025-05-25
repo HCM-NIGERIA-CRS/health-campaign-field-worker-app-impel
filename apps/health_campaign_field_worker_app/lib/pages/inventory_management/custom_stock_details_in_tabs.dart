76dec3f2d93e7658140a1aee5bb6cf95cced9494
amitverma-egov <amit.verma@egovernments.org>
2025-05-25 12:56:08 +0530

    stock downsync fix and set spaq1 and spaq2 count in tabs page

--- apps/health_campaign_field_worker_app/lib/pages/inventory_management/custom_stock_details_in_tabs.dart
+++ apps/health_campaign_field_worker_app/lib/pages/inventory_management/custom_stock_details_in_tabs.dart
@@ -927,9 +927,9 @@ class _DynamicTabsPageState extends LocalizedState<DynamicTabsPage>
 
         // Accumulate quantities based on product
         if (productName == Constants.spaq1) {
-          currentSpaq1Count += totalQty;
+          spaq1Count += totalQty;
         } else if (productName == Constants.spaq2) {
-          currentSpaq2Count += totalQty;
+          spaq2Count += totalQty;
         }
 
         context.read<RecordStockBloc>().add(
