e589462ed9733ae7c0eda226012fc942701f3472
amitverma-egov <amit.verma@egovernments.org>
2025-05-29 18:45:19 +0530

    remove duplicate repositories provider

--- apps/health_campaign_field_worker_app/lib/utils/constants.dart
+++ apps/health_campaign_field_worker_app/lib/utils/constants.dart
@@ -150,7 +150,6 @@ class Constants {
       ),
       LocationTrackerLocalBaseRepository(
           sql, LocationTrackerOpLogManager(isar)),
-      // StockLocalRepository(sql, StockOpLogManager(isar)),
       CustomStockLocalRepository(sql, StockOpLogManager(isar)),
       StockReconciliationLocalRepository(
         sql,
@@ -167,9 +166,7 @@ class Constants {
       TaskLocalRepository(sql, TaskOpLogManager(isar)),
       SideEffectLocalRepository(sql, SideEffectOpLogManager(isar)),
       ReferralLocalRepository(sql, ReferralOpLogManager(isar)),
-
       HFReferralLocalRepository(sql, HFReferralOpLogManager(isar)),
-
       AttendanceLocalRepository(
         sql,
         AttendanceOpLogManager(isar),
@@ -178,11 +175,6 @@ class Constants {
         sql,
         AttendanceLogOpLogManager(isar),
       ),
-
-      HFReferralLocalRepository(sql, HFReferralOpLogManager(isar)),
-
-      HFReferralLocalRepository(sql, HFReferralOpLogManager(isar)),
-
       ServiceDefinitionLocalRepository(
         sql,
         ServiceDefinitionOpLogManager(isar),
@@ -276,10 +268,6 @@ class Constants {
           AttendanceLogRemoteRepository(dio, actionMap: actions),
         if (value == DataModelType.complaints)
           PgrServiceRemoteRepository(dio, actionMap: actions),
-        if (value == DataModelType.hFReferral)
-          HFReferralRemoteRepository(dio, actionMap: actions),
-        if (value == DataModelType.hFReferral)
-          HFReferralRemoteRepository(dio, actionMap: actions),
         if (value == DataModelType.serviceDefinition)
           ServiceDefinitionRemoteRepository(dio, actionMap: actions),
         if (value == DataModelType.service)
--- apps/health_campaign_field_worker_app/lib/widgets/network_manager_provider_wrapper.dart
+++ apps/health_campaign_field_worker_app/lib/widgets/network_manager_provider_wrapper.dart
@@ -317,12 +317,6 @@ class NetworkManagerProviderWrapper extends StatelessWidget {
           ),
         ),
       ),
-      RepositoryProvider<LocalRepository<ServiceModel, ServiceSearchModel>>(
-        create: (_) => ServiceLocalRepository(
-          sql,
-          ServiceOpLogManager(isar),
-        ),
-      )
     ];
   }
 
