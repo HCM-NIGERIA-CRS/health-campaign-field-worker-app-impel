238182cbfb7699818a8f04b6052c33fdd2b2f2f0
psbskb <psb.skb22@gmail.com>
2025-06-27 13:27:26 +0530

    remove null values in previews

--- apps/health_campaign_field_worker_app/lib/pages/inventory_management/view_stock_records.dart
+++ apps/health_campaign_field_worker_app/lib/pages/inventory_management/view_stock_records.dart
@@ -101,6 +101,15 @@ class _ViewStockRecordsPageState extends LocalizedState<ViewStockRecordsPage>
         ?.value
         .toString();
 
+    String? batchNumber = stock.additionalFields?.fields
+        .firstWhere(
+          (field) => field.key == 'batchNumber',
+          orElse: () => const AdditionalField('batchNumber', ''),
+        )
+        .value;
+
+    String? wayBillNumber = stock.wayBillNumber;
+
     return SingleChildScrollView(
       padding: const EdgeInsets.all(16),
       child: Column(
@@ -175,31 +184,27 @@ class _ViewStockRecordsPageState extends LocalizedState<ViewStockRecordsPage>
                   const SizedBox(height: 12),
                   if (InventorySingleton().isDistributor != true) ...[
                     // Waybill Number
-                    InputField(
-                      type: InputType.text,
-                      label: 'Waybill Number *',
-                      initialValue: stock.wayBillNumber ?? '',
-                      isDisabled: true,
-                      readOnly: true,
-                    ),
-                    const SizedBox(height: 12),
+                    if (wayBillNumber != null && wayBillNumber.isNotEmpty)
+                      InputField(
+                        type: InputType.text,
+                        label: 'Waybill Number *',
+                        initialValue: wayBillNumber,
+                        isDisabled: true,
+                        readOnly: true,
+                      ),
+                    if (wayBillNumber != null && wayBillNumber.isNotEmpty)
+                      const SizedBox(height: 12),
                     // Batch Number
-                    InputField(
-                      type: InputType.text,
-                      label: 'Batch Number',
-                      initialValue: stock.additionalFields?.fields
-                              .firstWhere(
-                                (field) => field.key == 'batchNumber',
-                                orElse: () =>
-                                    const AdditionalField('batchNumber', ''),
-                              )
-                              .value
-                              ?.toString() ??
-                          '',
-                      isDisabled: true,
-                      readOnly: true,
-                    ),
-                    const SizedBox(height: 12),
+                    if (batchNumber != null && batchNumber.isNotEmpty)
+                      InputField(
+                        type: InputType.text,
+                        label: 'Batch Number',
+                        initialValue: batchNumber,
+                        isDisabled: true,
+                        readOnly: true,
+                      ),
+                    if (batchNumber != null && batchNumber.isNotEmpty)
+                      const SizedBox(height: 12),
                   ],
 
                   // Quantity
