7897105e21e794e385a01c2d932e58da35b326c4
amitverma-egov <amit.verma@egovernments.org>
2025-08-20 14:15:53 +0530

    reorder cards

--- apps/health_campaign_field_worker_app/lib/pages/home.dart
+++ apps/health_campaign_field_worker_app/lib/pages/home.dart
@@ -610,10 +610,10 @@ class _HomePageState extends LocalizedState<HomePage> {
 
     final homeItemsLabel = <String>[
       // INFO: Need to add items label of package Here
+      i18.home.dailyImplementationPlanLabel,
+      i18.home.campaignDeliverySelection,
       i18.home.mySurveyForm,
-
       i18.home.manageAttendanceLabel,
-
       i18.home.beneficiaryReferralLabel,
       // i18.home.beneficiaryLabel,
       i18.home.manageStockLabel,
@@ -622,11 +622,9 @@ class _HomePageState extends LocalizedState<HomePage> {
       i18.home.viewSummaryReportsLabel,
       i18.home.syncDataLabel,
       i18.home.fileComplaint,
-      i18.home.dailyImplementationPlanLabel,
-      i18.home.campaignDeliverySelection,
+      i18.home.beneficiaryIdLabel,
       i18.home.db,
       i18.home.dashboard,
-      i18.home.beneficiaryIdLabel,
     ];
 
     final List<String> filteredLabels = homeItemsLabel
