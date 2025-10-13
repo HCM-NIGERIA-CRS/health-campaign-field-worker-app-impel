8f4b8312c1e49d31fc32ba534cf9c27767593b28
amitverma-egov <amit.verma@egovernments.org>
2025-10-13 17:56:15 +0530

    fix bug issue for duplicate create oplog for user action in auto sync

--- apps/health_campaign_field_worker_app/lib/utils/constants.dart
+++ apps/health_campaign_field_worker_app/lib/utils/constants.dart
@@ -245,7 +245,7 @@ class Constants {
         ServiceOpLogManager(isar),
       ),
       UserActionLocalRepository(sql, UserActionOpLogManager(isar)),
-      CustomUserActionLocalRepository(sql, UserActionOpLogManager(isar)),
+      // CustomUserActionLocalRepository(sql, UserActionOpLogManager(isar)),
     ];
   }
 
